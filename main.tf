cee4755dbde4662cc332b589c0cd6862ae06dcc6
Rufus Ajiboye <ruufman@gmail.com>
2024-02-18 00:42:10 +0000

    Files updated and added

--- main.tf
+++ main.tf
@@ -26,3 +26,18 @@ resource "aws_instance" "AnsibleServer" {
     Name = "Ansible-Server"
   }
 }
+
+resource "aws_instance" "EKSServer" {
+
+  ami                         = "ami-0323d48d3a525fd18"
+  instance_type               = "t2.medium"
+  key_name                    = "test100"
+  subnet_id                   = aws_subnet.subnet_public1.id
+  vpc_security_group_ids      = [aws_security_group.web-traffic.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "EKS-Server"
+  }
+}
+
